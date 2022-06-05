d08536236599bdc3add1b23b38877a81f6910922
ashwinaadithan <ashwin21rr@gmail.com>
2022-06-06 00:12:19 +0530

    Initial Commit

---
+++ src/main/java/com/consultadd/demoapi/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.consultadd.demoapi.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Jake = new Student(
+                    "Jake",
+                    "jake@gmail.com",
+                     LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2005, Month.JANUARY, 22)
+            );
+            repository.saveAll(
+                    List.of(Jake, alex)
+            );
+        };
+    }
+}
